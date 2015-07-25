d6d904ab344635f9a80afa638cea2787c0c6a331
Cole Willsea <coleww@gmail.com>
2015-07-25 07:51:17 -0700

    chop up the odds a bit

--- prettify.js
+++ prettify.js
@@ -1,6 +1,8 @@
 var toUnicode = require("to-unicode")
 var diacrit = require("diacriticize")
 var font = ['circled', 'fullWidth', 'parenthesized', 'rockDots', 'smallCaps', 'stroked'][~~(Math.random() * 6)]
+
 module.exports = function(text){
-  return diacrit(toUnicode(text, font), 0.05)
+  // half the time, we add some diacritic marks. The other half, we use a unicode font
+  return (Math.random() < 0.5) ? diacrit(text, 0.15) : toUnicode(text, font)
 }
