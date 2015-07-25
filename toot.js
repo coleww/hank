d67adc45ef8d333dbdbd3ccdb1b158af1a01794f
Cole Willsea <coleww@gmail.com>
2015-07-25 10:33:05 -0400

    credddddds dont lose them i dont thik this box will ever t=push to my fitssssss git

--- toot.js
+++ toot.js
@@ -1,9 +1,9 @@
 var Twit = require("twit")
 var T = new Twit({
-    consumer_key: "SPIDERS!!!!"
-  , consumer_secret: "SPIDERS!!!!"
-  , access_token: "SPIDERS!!!!"
-  , access_token_secret: "SPIDERS!!!!"
+    consumer_key: "la2B2hL4PuQn3ShHc1llZtol6"
+  , consumer_secret: "n8MpdwDAa9m36hNmEehzwdbmRQEZavtuqcy2XDla8UkLB7WgPs"
+  , access_token: "2823130017-87Ne146opB4ONmJj7ySFJQfKcsOSTxzKGu8Q042"
+  , access_token_secret: "YwVljy4NTi1kO8wsbkHR1P1PsBrPqVY8wiiqlaQBMmDwQ"
 })
 
 module.exports = function(text){
