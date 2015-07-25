6cf92ea66ffa4d53cba98886911895da0da803e9
Cole Willsea <coleww@gmail.com>
2015-07-25 09:46:53 -0700

    oh i didnt actually commit these things before MY BAD

--- destructure.js
+++ destructure.js
@@ -1,6 +1,7 @@
 var nPlus = require("n-plus-7")
 var wordVomit = require("word-vomit")
 var poeticVomit = require("poetic-vomit")
+var newSlang = require("new-slang")
 
 module.exports = function(text){
   // roll some dice and maybe perform some transforms
@@ -9,6 +10,7 @@ module.exports = function(text){
   if (Math.random() < the_odds) toot = nPlus(toot)
   if (Math.random() < the_odds) toot = poeticVomit(toot, 0.33)
   if (Math.random() < the_odds) toot = wordVomit(toot, 0.33)
+  if (Math.random() < the_odds) toot = newSlang(toot, 0.33)
 
   return toot
 }
